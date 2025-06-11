9305c9863897359d81fc2a1d989c62c72fee47df
ShahHarshvi21 <harshvi.shah@zeuslearning.com>
2025-06-11 15:49:02 +0530

    task2

---
+++ Task1/script.js
@@ -0,0 +1,33 @@
+function f1(){
+    const nameInput=document.getElementById('name');
+    const name1=nameInput.value.trim()
+    console.log(name1);
+
+    const commentInput=document.getElementById('comments');
+    const comment1=commentInput.value.trim()
+    console.log(comment1);
+
+    if(!name1 && !comment1){
+        alert("Please fill name and comment field");
+        nameInput.focus();
+        return false;
+    }
+    else if(!name1){
+        alert("Please fill name field");
+        nameInput.focus();
+        return false;
+    }
+    else if(!comment1){
+        alert("Please fill comment field");
+        commentInput.focus();
+        return false;
+    }
+    const gender = document.querySelector('input[name="gender"]:checked');
+    if (gender) {
+        console(gender.value);
+    } else {
+        alert("Enter gender");
+        return false;
+    }
+    return true;
+}
